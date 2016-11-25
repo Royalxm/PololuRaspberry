732c374c1a95b11649151a91b00ec2c0580bb052
Royalxm <royalxm@hotmail.fr>
2016-11-25 15:07:15 +0100

    Communication avec le webs ervice OKAY

--- webservice.py
+++ webservice.py
@@ -1,6 +1,7 @@
 import pyrebase
 import RPi.GPIO as GPIO
 from picamera import PiCamera
+import time
 
 #Configuration Firebase
 config = {
@@ -13,7 +14,7 @@ config = {
 
 firebase = pyrebase.initialize_app(config)
 auth = firebase.auth()
-user = auth.sign_in_with_email_and_password("ro@ro.com", "azerty")
+user = auth.sign_in_with_email_and_password("", "")
 db = firebase.database()
 storage = firebase.storage()
 print ("database OKAY")
@@ -21,11 +22,10 @@ print ("database OKAY")
 #Configuration GPIO
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
-GPIO.setup(14, GPIO.OUT, initial=GPIO.LOW)# broche 12 est une sortie initialement a l'etat haut
-GPIO.setup(15, GPIO.OUT, initial=GPIO.LOW)# broche 12 est une sortie initialement a l'etat haut
+GPIO.setup(14, GPIO.OUT, initial=GPIO.LOW)
+GPIO.setup(15, GPIO.OUT, initial=GPIO.LOW)
 
-
-
+format = "%Y%m%d%H%M%S"
 #Configuration camera
 
 camera = PiCamera()
@@ -33,29 +33,26 @@ camera.resolution = (1024, 768)
 #camera.start_preview()
 # Camera warm-up time
 #sleep(2)
-def control_robot(data):
 
-	if data['Avance'] == 1:
-		GPIO.output(14, GPIO.HIGH)
-	else:
-		GPIO.output(14, GPIO.LOW)
+def control_robot(data,user):
 
-	if data['Recule'] == 1:
-		GPIO.output(15, GPIO.HIGH)
-	else:
-		GPIO.output(15, GPIO.LOW)
+        if data['Avance'] == 1:
+                GPIO.output(14, GPIO.HIGH)
+        else:
+                GPIO.output(14, GPIO.LOW)
 
-	if data['Photo'] == 1:
-		camera.capture('images/example.jpg')
-		storage.child("images/example.jpg").put("ImageTest/ex.jpg",user['idToken'])
+        if data['Recule'] == 1:
+                GPIO.output(15, GPIO.HIGH)
+        else:
+                GPIO.output(15, GPIO.LOW) 
 
-	
+        if data['Photo'] == 1:
+                pictureName = time.strftime("%Y%m%d%H%M%S")
+                camera.capture('images/' +pictureName+ '.jpg')
+                storage.child('images/' +pictureName+ '.jpg').put('images/' +pictureName+ '.jpg',user['idToken'])
+                db.child("RobotControl").update({"Photo":"0"},user['idToken'])
 
 
 while 1:
-
-	data = db.child("RobotControl").get(user['idToken'])
-	control_robot(data.val())
-
-
-
+        data = db.child("RobotControl").get(user['idToken'])
+        control_robot(data.val(),user)
